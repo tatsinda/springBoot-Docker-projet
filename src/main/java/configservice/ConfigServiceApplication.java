99e2d317660eb2f94cf153bcbcaca36a7a424094
tatsinda <blancleroytatsinda@gmail.com>
2022-07-27 04:18:27 +0200

    first commit

---
+++ src/main/java/configservice/ConfigServiceApplication.java
@@ -0,0 +1,23 @@
+package configservice;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServiceApplication {
+
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServiceApplication.class, args);
+
+		
+
+
+	}
+
+}
